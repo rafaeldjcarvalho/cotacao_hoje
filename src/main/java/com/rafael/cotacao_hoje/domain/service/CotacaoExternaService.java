4ce1460dbfa8cd680a25978eb550437df3e2bb16
Rafael de Jesus Carvalho <141766102+rafaeldjcarvalho@users.noreply.github.com>
2025-09-02 11:38:51 -0300

    feat: Update CotacaoExternaService

--- src/main/java/com/rafael/cotacao_hoje/domain/service/CotacaoExternaService.java
+++ src/main/java/com/rafael/cotacao_hoje/domain/service/CotacaoExternaService.java
@@ -1,15 +1,20 @@
 package com.rafael.cotacao_hoje.domain.service;
 
-import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import com.rafael.cotacao_hoje.domain.dto.CotacaoDTO;
+import com.rafael.cotacao_hoje.domain.dto.MoedaDTO;
+import com.rafael.cotacao_hoje.domain.dto.mapper.CotacaoMapper;
 import com.rafael.cotacao_hoje.domain.entity.Cotacao;
+import com.rafael.cotacao_hoje.domain.repository.CotacaoRepository;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 @Service
 public class CotacaoExternaService {
@@ -17,24 +22,42 @@ public class CotacaoExternaService {
 	@Autowired
 	private WebClient webClient;
 	
+	@Autowired
+	private CotacaoMapper cotacaoMapper;
+	
+	@Autowired
+	private CotacaoRepository cotacaoRepository;
+	
 	//private String moedasSuportadas = "USD-BRL,EUR-BRL,BTC-BRL";
 	
-	// Trocar Flux<CotacaoDTO> por List<Cotacao>
-	// Precisa salvar os dados no BD
-	public Flux<CotacaoDTO> buscarCotacoesAtuais(String pares) {
-		Flux<CotacaoDTO> listaAtual = webClient.get()
+	public Flux<Cotacao> buscarCotacoesAtuais(String pares) {
+		return webClient.get()
 				.uri("last/" + pares)
 				.retrieve()
-				.bodyToFlux(CotacaoDTO.class);
-		return listaAtual;
+				.bodyToMono(new ParameterizedTypeReference<Map<String, MoedaDTO>>() {})
+				.flatMapMany(map -> Flux.fromIterable(map.entrySet()))
+				.map(entry -> cotacaoMapper.toEntity(entry.getKey(), entry.getValue()))
+				.flatMap(this::salvarCotacao);
 	}
 	
-	public Flux<CotacaoDTO> buscarHistoricoDiario(String par, int dias) {
-		Flux<CotacaoDTO> listaAtual = webClient.get()
+	public Flux<Cotacao> buscarHistoricoDiario(String par, int dias) {
+		return this.buscarHistorico(par, dias)
+				.map(dto -> cotacaoMapper.fromHistorico(dto, par));
+	}
+	
+	private Flux<MoedaDTO> buscarHistorico(String par, int dias) {
+		return webClient.get()
 				.uri("daily/" + par + "/" + dias)
 				.retrieve()
-				.bodyToFlux(CotacaoDTO.class);
-		return listaAtual;
+				.bodyToFlux(MoedaDTO.class);
+	}
+	
+	/* Esse método funciona, mas misturar JPA com WebFlux não é recomendável 
+	 * em produção por questões de desempenho e controle de recursos.
+	 */
+	public Mono<Cotacao> salvarCotacao(Cotacao cotacao) {
+		return Mono.fromCallable(() -> cotacaoRepository.save(cotacao))
+	               .subscribeOn(Schedulers.boundedElastic()); // Executa fora do thread principal
 	}
 
 }
