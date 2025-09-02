9bc36befc08ed78f53ce3e856e5f457cbf87a3d2
Rafael de Jesus Carvalho <141766102+rafaeldjcarvalho@users.noreply.github.com>
2025-09-02 15:01:31 -0300

    feat: Update ApplicationClass

--- src/main/java/com/rafael/cotacao_hoje/CotacaoHojeApplication.java
+++ src/main/java/com/rafael/cotacao_hoje/CotacaoHojeApplication.java
@@ -2,8 +2,10 @@ package com.rafael.cotacao_hoje;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CotacaoHojeApplication {
 
 	public static void main(String[] args) {
