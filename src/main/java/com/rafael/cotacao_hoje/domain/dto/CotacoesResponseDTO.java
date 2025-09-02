780ae7628d759a894bf142badf9924332e135972
Rafael de Jesus Carvalho <141766102+rafaeldjcarvalho@users.noreply.github.com>
2025-09-02 11:38:05 -0300

    feat: Create CotacoesResponseDTO

---
+++ src/main/java/com/rafael/cotacao_hoje/domain/dto/CotacoesResponseDTO.java
@@ -0,0 +1,7 @@
+package com.rafael.cotacao_hoje.domain.dto;
+
+import java.util.HashMap;
+
+public class CotacoesResponseDTO extends HashMap<String, MoedaDTO> {
+	private static final long serialVersionUID = 1L;
+}
