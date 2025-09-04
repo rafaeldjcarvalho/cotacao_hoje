96d94daba0039654959048fec497dcb1683aba5f
Rafael de Jesus Carvalho <141766102+rafaeldjcarvalho@users.noreply.github.com>
2025-09-04 13:32:18 -0300

    feat: Create CorsConfig

---
+++ src/main/java/com/rafael/cotacao_hoje/domain/infra/security/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.rafael.cotacao_hoje.domain.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    // Permite os domínios do frontend Angular e React
+                    .allowedOrigins(
+                        "http://localhost:4200",  // Angular
+                        "http://localhost:3000"   // React
+                    )
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+            }
+        };
+    }
+}
